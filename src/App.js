096fc40fd8fdda254ab034591f4d27b1625ba5c7
marinodilauro <m.dilauro90@gmail.com>
2024-08-01 12:48:20 +0200

    Add list item component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
-import "./style.css";
+
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
 
 export default function App() {
   return (
@@ -36,7 +41,21 @@ function Form() {
 // List component
 function PackingList() {
   return (
-    <div className="list"> LIST </div>
+    <div className="list">
+      <ul>
+        {initialItems.map(item => <Item item={item} />)}
+      </ul>
+    </div>
+  );
+}
+
+// List item component
+function Item({ item }) {
+  return (
+    <li>
+      <span>{item.quantity} {item.description}</span>
+      <button>&times;</button>
+    </li>
   );
 }
 
