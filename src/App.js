763fe5e0ccfae5eb7c2d6ce4daca3714f275ec2e
marinodilauro <m.dilauro90@gmail.com>
2024-08-01 15:18:54 +0200

    Add form submit function

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
@@ -31,14 +32,31 @@ function Logo() {
 
 // Form component
 function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = {
+      id: Date.now(),
+      description,
+      quantity,
+      packed: false
+    };
+    console.log(newItem)
+  }
+
   return (
-    <form className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip?</h3>
-      <select>
+      <select value={quantity} onChange={e => setQuantity(Number(e.target.value))}>
         {Array.from({ length: 20 }, (_, i) => i + 1).map(num => <option value={num} key={num}>{num}</option>)}
       </select>
-      <input type="text" placeholder="Item..." />
-      <button>Add</button>
+      <input type="text" placeholder="Item..." value={description} onChange={e => setDescription(e.target.value)} />
+      <button type="submit">Add</button>
     </form>
   );
 }
