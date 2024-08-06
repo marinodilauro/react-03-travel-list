29172445c26025c7260905645c4928a10d5cfadc
marinodilauro <m.dilauro90@gmail.com>
2024-08-06 12:33:59 +0200

    Add PWA files

--- src/index.js
+++ src/index.js
@@ -10,3 +10,10 @@ root.render(
   </React.StrictMode>
 );
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then((reg) => console.log('Success: ', reg.scope))
+      .catch((err) => console.log('Failure: ', err));
+  })
+}
